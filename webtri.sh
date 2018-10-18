ce922c6d9af77583693d7a8836d04ff181d33b35
phil8192 <phil@parasec.net>
2018-10-18 18:39:00 +0000

    parse args and refactor

--- webtri.sh
+++ webtri.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+# ==============================================================================
+# Highways England webtris API shell client.
+# ------------------------------------------------------------------------------
+# http://webtris.highwaysengland.co.uk/
+# http://webtris.highwaysengland.co.uk/api/swagger/ui/index
+# ------------------------------------------------------------------------------
+# Phil Stubbings <phil@parasec.net>
+# https://github.com/phil8192
+# ==============================================================================
 
+# Conf.
 ENDPOINT="http://webtris.highwaysengland.co.uk/api/v1.0"
 MAX_ROWS=40000
 JQ=true
-
 
 # Internal: Get seconds since epoch for ddmmyyyy formated date.
 #
@@ -18,12 +27,12 @@ JQ=true
 #
 # Returns seconds since the epoch.
 _seconds_since_epoch() {
-	if [ "$(uname)" = "Linux"  ] ;then
-		date -u -d "$(echo "$1" \
-				|awk '{print substr($1,5,4) substr($1,3,2) substr($1,1,2)}')" +%s
-	else
-		date -u -j -f "%d%m%Y:%H:%M:%S" "$1:00:00:00" +%s
-	fi
+  if [ "$(uname)" = "Linux"  ] ;then
+    date -u -d "$(echo "$1" \
+        |awk '{print substr($1,5,4) substr($1,3,2) substr($1,1,2)}')" +%s
+  else
+    date -u -j -f "%d%m%Y:%H:%M:%S" "$1:00:00:00" +%s
+  fi
 }
 
 # Internal: Get overall quality for specified sites.
@@ -43,25 +52,25 @@ _seconds_since_epoch() {
 #
 #   * quality
 _get_overall_quality() {
-	sites=$1
-	start=$2
-	end=$3
+  sites=$1
+  start=$2
+  end=$3
 
-	raw=$(curl -s -X GET --header 'Accept: application/json' \
-			"$ENDPOINT/quality/overall?sites=$sites&start_date=$start&end_date=$end")
+  raw=$(curl -s -X GET --header 'Accept: application/json' \
+      "$ENDPOINT/quality/overall?sites=$sites&start_date=$start&end_date=$end")
 
-	if [ "$JQ" = true ] ;then
-		start_secs=$(_seconds_since_epoch "$start")
-		end_secs=$(_seconds_since_epoch "$end")
-		days=$((1 + ((end_secs - start_secs) / 86400)))
-		echo "quality"
-		printf "%.f\\n" "$(echo "$raw" \
-				|jq -r --arg days $days \
-				'.data_quality * (([$days |tonumber, 2] |max) -1) / ($days |tonumber)')"
-	else
-		# note, does not correct for remote bug.
-		echo "$raw"
-	fi
+  if [ "$JQ" = true ] ;then
+    start_secs=$(_seconds_since_epoch "$start")
+    end_secs=$(_seconds_since_epoch "$end")
+    days=$((1 + ((end_secs - start_secs) / 86400)))
+    echo "quality"
+    printf "%.f\\n" "$(echo "$raw" \
+        |jq -r --arg days $days \
+        '.data_quality * (([$days |tonumber, 2] |max) -1) / ($days |tonumber)')"
+  else
+    # note, does not correct for remote bug.
+    echo "$raw"
+  fi
 }
 
 # Internal: Get daily quality for specified site.
@@ -81,19 +90,19 @@ _get_overall_quality() {
 #   * date
 #   * quality
 _get_daily_quality() {
-	sites=$1
-	start=$2
-	end=$3
-	raw=$(curl -s -X GET --header 'Accept: application/json' \
-			"$ENDPOINT/quality/daily?siteId=$sites&start_date=$start&end_date=$end")
-	if [ "$JQ" = true ] ;then
-		echo "date,quality"
-		echo "$raw" \
-				|jq -r '.Qualities |.[] |map(.) |@csv' \
-				|sed 's/\"//g'
-	else
-		echo "$raw"
-	fi
+  sites=$1
+  start=$2
+  end=$3
+  raw=$(curl -s -X GET --header 'Accept: application/json' \
+      "$ENDPOINT/quality/daily?siteId=$sites&start_date=$start&end_date=$end")
+  if [ "$JQ" = true ] ;then
+    echo "date,quality"
+    echo "$raw" \
+        |jq -r '.Qualities |.[] |map(.) |@csv' \
+        |sed 's/\"//g'
+  else
+    echo "$raw"
+  fi
 }
 
 # Public: Get an area bounding box.
@@ -120,15 +129,15 @@ _get_daily_quality() {
 #   * y_lon
 #   * y_lat
 get_area() {
-	id=$1
-	raw=$(curl -s -X GET --header 'Accept: application/json' \
-			"$ENDPOINT/areas/$id")
-	if [ "$JQ" = true ] ;then
-		echo "id,name,description,x_lon,x_lat,y_lon,y_lat"
-		echo "$raw" |jq -r '.areas? |.[] // [] |join(",")'
-	else
-		echo "$raw"
-	fi
+  id=$1
+  raw=$(curl -s -X GET --header 'Accept: application/json' \
+      "$ENDPOINT/areas/$id")
+  if [ "$JQ" = true ] ;then
+    echo "id,name,description,x_lon,x_lat,y_lon,y_lat"
+    echo "$raw" |jq -r '.areas? |.[] // [] |join(",")'
+  else
+    echo "$raw"
+  fi
 }
 
 # Public: Get overall or daily quality.
@@ -157,19 +166,19 @@ get_area() {
 #   * date,quality (daily) or
 #   * quality (overall)
 get_quality() {
-	sites=$1
-	start=$2
-	end=$3
-	breakdown=$4
+  sites=$1
+  start=$2
+  end=$3
+  breakdown=$4
 
-	if [ "$breakdown" = "overall" ] ;then
-		_get_overall_quality "$sites" "$start" "$end"
-	elif [ "$breakdown" = "daily" ] ;then
-		_get_daily_quality "$sites" "$start" "$end"
-	else
-		echo "=== error ===" >&2
-		echo "expected 'overall' or 'daily'" >&2
-	fi
+  if [ "$breakdown" = "overall" ] ;then
+    _get_overall_quality "$sites" "$start" "$end"
+  elif [ "$breakdown" = "daily" ] ;then
+    _get_daily_quality "$sites" "$start" "$end"
+  else
+    echo "=== error ===" >&2
+    echo "expected 'overall' or 'daily'" >&2
+  fi
 }
 
 # Public: Get site report.
@@ -215,47 +224,47 @@ get_quality() {
 #   * speed_avg_mph
 #   * total_vol
 get_report() {
-	site_id=$1
-	interval=$2 # daily, monthly, annual
-	start=$3
-	end=$4
+  site_id=$1
+  interval=$2 # daily, monthly, annual
+  start=$3
+  end=$4
 
-	href="${ENDPOINT}/reports/${interval}?sites=${site_id}&start_date=${start}&end_date=${end}&page=1&page_size=$MAX_ROWS"
+  href="${ENDPOINT}/reports/${interval}?sites=${site_id}&start_date=${start}&end_date=${end}&page=1&page_size=$MAX_ROWS"
 
-	if [ "$JQ" = true ] ;then
-		printf "site_name,report_date,time_period_end,interval,"
-		printf "len_0_520_cm,len_521_660_cm,len_661_1160_cm,len_1160_plus_cm,"
-		printf "speed_0_10_mph,speed_11_15_mph,speed_16_20_mph,speed_21_25_mph,"
-		printf "speed_26_30_mph,sped_31_35_mph,speed_36_40_mph,speed_41_45_mph,"
-		printf "speed_46_50_mph,speed_51_55_mph,speed_56_60_mph,speed_61_70_mph,"
-		printf "speed_71_80_mph,speed_80_plus_mph,speed_avg_mph,total_vol\\n"
-	else
-		echo "["
-	fi
+  if [ "$JQ" = true ] ;then
+    printf "site_name,report_date,time_period_end,interval,"
+    printf "len_0_520_cm,len_521_660_cm,len_661_1160_cm,len_1160_plus_cm,"
+    printf "speed_0_10_mph,speed_11_15_mph,speed_16_20_mph,speed_21_25_mph,"
+    printf "speed_26_30_mph,sped_31_35_mph,speed_36_40_mph,speed_41_45_mph,"
+    printf "speed_46_50_mph,speed_51_55_mph,speed_56_60_mph,speed_61_70_mph,"
+    printf "speed_71_80_mph,speed_80_plus_mph,speed_avg_mph,total_vol\\n"
+  else
+    echo "["
+  fi
 
-	while [ "$href" ] ;do
-		raw=$(curl -s -X GET --header "Accept: application/json" "$href")
-		if [ "$(echo "$raw" |jq -r 'type')" != "object" ] ;then
-			echo "=== error ===" >&2
-			echo "$raw" >&2
-			exit 1
-		fi
-		href=$(echo "$raw" \
-				|jq -r '.Header.links |.[] |select(.rel == "nextPage") |.href')
-		if [ "$JQ" = true ] ;then
-			echo "$raw" |jq -r '.Rows |.[] |join(",")'
-		else
-			if [ "$href" ] ;then
-				echo "$raw,"
-			else
-				echo "$raw"
-			fi
-		fi
-	done
+  while [ "$href" ] ;do
+    raw=$(curl -s -X GET --header "Accept: application/json" "$href")
+    if [ "$(echo "$raw" |jq -r 'type')" != "object" ] ;then
+      echo "=== error ===" >&2
+      echo "$raw" >&2
+      exit 1
+    fi
+    href=$(echo "$raw" \
+        |jq -r '.Header.links |.[] |select(.rel == "nextPage") |.href')
+    if [ "$JQ" = true ] ;then
+      echo "$raw" |jq -r '.Rows |.[] |join(",")'
+    else
+      if [ "$href" ] ;then
+        echo "$raw,"
+      else
+        echo "$raw"
+      fi
+    fi
+  done
 
-	if [ "$JQ" = false ] ;then
-		echo "]"
-	fi
+  if [ "$JQ" = false ] ;then
+    echo "]"
+  fi
 }
 
 # Public: Get sites.
@@ -279,17 +288,17 @@ get_report() {
 #   * latitude
 #   * status
 get_sites() {
-	site_ids=$1
-	raw=$(curl -s -X GET --header 'Accept: application/json' \
-			"$ENDPOINT/sites/$site_ids")
-	if [ "$JQ" = true ] ;then
-		echo "id,name,description,longitude,latitude,status"
-		echo "$raw" \
-				|jq -r '.sites | .[] | map(.) |@csv' \
-				|sed 's/\"//g'
-	else
-		echo "$raw"
-	fi
+  site_ids=$1
+  raw=$(curl -s -X GET --header 'Accept: application/json' \
+      "$ENDPOINT/sites/$site_ids")
+  if [ "$JQ" = true ] ;then
+    echo "id,name,description,longitude,latitude,status"
+    echo "$raw" \
+        |jq -r '.sites | .[] | map(.) |@csv' \
+        |sed 's/\"//g'
+  else
+    echo "$raw"
+  fi
 }
 
 # Public: Get site types.
@@ -305,13 +314,14 @@ get_sites() {
 #   * id
 #   * description
 get_site_types() {
-	raw=$(curl -s -X GET --header 'Accept: application/json' "$ENDPOINT/sitetypes")
-	if [ "$JQ" = true ] ;then
-		echo "id,description"
-		echo "$raw" |jq -r '.sitetypes | .[] |join(",")'
-	else
-		echo "$raw"
-	fi
+  raw=$(curl -s -X GET --header 'Accept: application/json' \
+      "$ENDPOINT/sitetypes")
+  if [ "$JQ" = true ] ;then
+    echo "id,description"
+    echo "$raw" |jq -r '.sitetypes | .[] |join(",")'
+  else
+    echo "$raw"
+  fi
 }
 
 # Public: Get sites by type.
@@ -346,26 +356,65 @@ get_site_types() {
 #   * latitude
 #   * status
 get_site_by_type() {
-	site_type=$1
-	raw=$(curl -s -X GET --header 'Accept: application/json' "$ENDPOINT/sitetypes/$site_type/sites")
-	if [ "$JQ" = true ] ;then
-		echo "id,name,description,longitude,latitude,status"
-		echo "$raw" \
-				|jq -r '.sites | .[] | map(.) |@csv' \
-				|sed 's/\"//g'
-	else
-		echo "$raw"
-	fi
+  site_type=$1
+  raw=$(curl -s -X GET --header 'Accept: application/json' \
+      "$ENDPOINT/sitetypes/$site_type/sites")
+  if [ "$JQ" = true ] ;then
+    echo "id,name,description,longitude,latitude,status"
+    echo "$raw" \
+        |jq -r '.sites | .[] | map(.) |@csv' \
+        |sed 's/\"//g'
+  else
+    echo "$raw"
+  fi
 }
 
-#get_area 1
-#get_area
-#get_quality 5688 01012018 04012018 daily
-#get_quality 5688,5699 01012018 04012018 overall
-#get_report 5688 daily 01012015 01012018
-#get_report 5688 daily 01012018 01012018
-#get_sites
-#get_sites 5688
-#get_sites 5688,5689
-#get_site_by_type
-#get_site_by_type 1
+usage() {
+  echo "$0 -f <function> -a \"<args>\""
+  echo
+  echo "where <function is one of:"
+  echo "area, quality, report,sites, site_types, site_by_type"
+  echo
+  echo "and <args> should be inclosed in double quotes."
+}
+
+while getopts "hf:a:" opt; do
+  case $opt in
+    f)
+      fun=$OPTARG
+      ;;
+    a)
+      args=$OPTARG
+      ;;
+    h)
+      usage
+      exit 0
+    ;;
+    \?)
+      usage
+      exit 1
+    ;;
+  esac
+done
+
+if [ -z "$fun" ] || [ -z "$args" ]; then
+  if [ "$0" = "./webtri.sh" ] ;then
+    usage
+    exit 1
+  fi
+else
+  if [ "$fun" != "get_area"         ] &&
+     [ "$fun" != "get_quality"      ] &&
+     [ "$fun" != "get_report"       ] &&
+     [ "$fun" != "get_sites"        ] &&
+     [ "$fun" != "get_site_types"   ] &&
+     [ "$fun" != "get_site_by_type" ] ;then
+    echo "Must specify:" >&2
+    echo "get_{area,quality,report,sites,site_types,site_by_type}" >&2
+    exit 1
+  else
+    # need to parse args in a nicer way.
+    # (this will of course fail shellcheck)
+    $fun $args
+  fi
+fi
